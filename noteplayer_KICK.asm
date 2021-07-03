5461ea43b0eec634e99b3342b4829710c971c482
mikroman <65836036+mikroman@users.noreply.github.com>
2021-07-03 12:01:49 -0700

    Add files via upload

--- noteplayer_KICK.asm
+++ noteplayer_KICK.asm
@@ -5,15 +5,16 @@
 .label psthb = $fe
 *=$0401        //pet build
         .byte $0b,$04
-//###########  TOGGLE COMMENTS TO BUILD FOR PET (above) OR C64  #################
-//*=$0801         //c64/debugger build
-//        byte $0b,$08
-//.label acr = $DD0B
-//.label shregister = $DD0A
-//.label timer2 = $DD08
-//.label pstlb = $fd
-//.label psthb = $fe
-//###############################################################################
+//###########  TOGGLE BLOCK COMMENTS TO BUILD FOR PET (above) OR C64  ############
+/* *=$0801         //c64/debugger build
+         .byte $0b,$08
+.label acr = $DD0B
+.label shregister = $DD0A
+.label timer2 = $DD08
+.label pstlb = $fd
+.label psthb = $fe
+ */
+ //###############################################################################
         .byte $0a,$00,$9e,$34,$30,$39,$36,$00
         .byte $00,$00
 
